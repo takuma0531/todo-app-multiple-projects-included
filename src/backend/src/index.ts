50a82ba095c36bf2f9ffe54b43f542ca3cd2c0b2
ryuichi24 <ryuichi.nishi24@gmail.com>
2021-02-08 23:53:32 +0200

    initial running server

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,1 +1,24 @@
 
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+
+
+const app = express();
+
+// enable dotenv
+config();
+
+
+// add middleware
+app.use(express.json());
+app.use(cors());
+
+// base point
+app.get('/', (_: Request, res: Response) =>
+  res.status(200).send('API is running!')
+);
+
+// init server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
