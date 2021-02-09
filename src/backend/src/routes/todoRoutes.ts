2ed81435cc4fef0e970c996d061267a8251567ce
ryuichi24 <ryuichi.nishi24@gmail.com>
2021-02-09 23:09:32 +0200

    removed unneeded codes

--- src/backend/src/routes/todoRoutes.ts
+++ src/backend/src/routes/todoRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { userController, todoController } from '../controllers';
+import { todoController } from '../controllers';
 
 const router = Router();
 
@@ -8,11 +8,5 @@ router.get('/:id', todoController.getOneById);
 router.post('/', todoController.createOne);
 router.delete('/:id', todoController.deleteOne);
 router.put('/:id', todoController.updateOne);
-
-router.get('/', userController.getAll);
-router.get('/:id', userController.getOneById);
-router.post('/', userController.createOne);
-router.delete('/:id', userController.deleteOne);
-router.put('/:id', userController.updateOne);
 
 export default router;
