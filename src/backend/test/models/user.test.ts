111d73bcd9df2479989d61f8f8bbb4b12c920e71
ryuichi24 <ryuichi.nishi24@gmail.com>
2021-02-11 19:47:59 +0200

    update user model test

--- src/backend/test/models/user.test.ts
+++ src/backend/test/models/user.test.ts
@@ -1,6 +1,6 @@
 import { describe } from 'mocha';
 import { assert } from 'chai';
-import { connection } from 'mongoose'
+import { connection } from 'mongoose';
 
 import { connectDB } from '../../src/data-access/connect-db';
 import { User } from '../../src/data-access/models';
@@ -13,15 +13,14 @@ describe('User', () => {
 
   beforeEach(async () => {
     await connection.collections.users.drop();
-});
+  });
 
   it('should save a new user in the db', async () => {
-
     const newUser = User.new({
-      username: 'test user 1',
-      email: 'testuser1@example.com',
-      password: 'testuser1password',
-      phone: '000011112222',
+      username: 'testuser',
+      email: 'testuser@example.com',
+      password: 'testuser@password',
+      phone: '000000001',
       avatar: '',
       gender: 'm',
       roles: ['user'],
@@ -31,5 +30,71 @@ describe('User', () => {
 
     // if it is new, the user is not saved
     assert(!savedUser.isNew);
+  });
+
+  it('should get one saved user from the db', async () => {
+    const newUser = User.new({
+      username: 'testuser',
+      email: 'testuser@example.com',
+      password: 'testuser@password',
+      phone: '000000000',
+      avatar: '',
+      gender: 'm',
+      roles: ['user'],
+    });
+
+    const savedUser = await newUser.save();
+
+    const fetchedUser = await User.findById(savedUser._id);
+
+    // convert them into string because mongodb ObjectId cannot be asserted
+    const savedUserId = String(savedUser._id);
+    const fetchedUserId = String(fetchedUser?._id);
+
+    assert.equal(savedUserId, fetchedUserId);
+  });
+
+  it('should delete one user in the db by the user id', async () => {
+    const newUser = User.new({
+      username: 'testuser',
+      email: 'testuser@example.com',
+      password: 'testuser@password',
+      phone: '000000000',
+      avatar: '',
+      gender: 'm',
+      roles: ['user'],
+    });
+
+    const savedUser = await newUser.save();
+
+    await User.findByIdAndRemove(savedUser._id);
+
+    const fetchedUser = await User.findById(savedUser._id);
+
+    assert.equal(fetchedUser, null);
+  });
+
+  it('should update "username" of one user in the db by the user id', async () => {
+    const newUser = User.new({
+      username: 'testuser',
+      email: 'testuser@example.com',
+      password: 'testuser@password',
+      phone: '000000000',
+      avatar: '',
+      gender: 'm',
+      roles: ['user'],
+    });
+
+    const savedUser = await newUser.save();
+
+    const newUsername = 'testuser (updated)';
+
+    await User.findByIdAndUpdate(savedUser._id, {
+      username: newUsername,
+    });
+
+    const fetchedUser = await User.findOne({ username: newUsername });
+
+    assert.equal(fetchedUser?.username, newUsername);
   });
 });
