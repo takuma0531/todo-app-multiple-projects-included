0995d3aeddf19cdd226bd43cbfe4e3cab003f39d
takuma masaki <tb.masaki0531@gmail.com>
2022-07-23 16:26:43 +0900

    add navigate to go back

--- front-end/to-do-app-react/src/components/category-container.tsx
+++ front-end/to-do-app-react/src/components/category-container.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useTodoList } from "../hooks/useTodoList";
 import { TodoListView } from "./todo-list-view";
 import { AddButtonView } from "./add-button-view";
 
 export const CategoryContainer = () => {
   const { todos, openToAdd } = useTodoList();
+  const navigate = useNavigate();
 
   return (
     <div>
+      <button onClick={() => navigate(-1)}>Go back</button>
       <AddButtonView text={"+ New Todo"} startFunc={openToAdd} />
       <TodoListView todos={todos} />
     </div>
