1f21dfe7a71b64e4e19b2293ebc7b7809ce63d7f
takuma masaki <tb.masaki0531@gmail.com>
2022-07-27 13:47:26 +0900

    apply todo container

--- front-end/to-do-app-react/src/components/todo-list-view.tsx
+++ front-end/to-do-app-react/src/components/todo-list-view.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Todo } from "../models/todo-model";
+import { TodoContainer } from "./todo-container";
 
 interface Props {
   todos: Todo[];
@@ -9,15 +10,7 @@ interface Props {
 export const TodoListView = ({ todos }: Props) => {
   const renderTodos = (todos: Todo[]) =>
     todos.map((todo: Todo, index: number) => {
-      // console.log(todo.createdAt);
-
-      return (
-        <li key={index}>
-          {todo.content}
-          <span>{/* createdAt */}</span>
-          <span>...</span>
-        </li>
-      );
+      return <TodoContainer todo={todo} index={index} key={index} />;
     });
 
   return (
