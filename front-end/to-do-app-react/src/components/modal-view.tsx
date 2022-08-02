c667f7a76a78e97be93144f45aae9279ed8266fa
takuma masaki <tb.masaki0531@gmail.com>
2022-08-02 15:24:27 +0900

    modal in progress

--- front-end/to-do-app-react/src/components/modal-view.tsx
+++ front-end/to-do-app-react/src/components/modal-view.tsx
@@ -10,12 +10,55 @@ interface Props {
 export function ModalView({ isVisible, setIsVisible, children }: Props) {
   return isVisible ? (
     <ModalViewElement>
-      <button onClick={() => setIsVisible()}>Close</button>
-      {children}
+      <div className={"inner-modal"}>
+        <button className={"close-button"} onClick={() => setIsVisible()}>
+          ×
+        </button>
+        {children}
+      </div>
     </ModalViewElement>
   ) : (
     <div></div>
   );
 }
 
-const ModalViewElement = styled.div``;
+const ModalViewElement = styled.div`
+  position: fixed;
+  top: 0%;
+  left: 0%;
+  width: 100vw;
+  height: 100vh;
+  background: #0000004d;
+  z-index: 1;
+
+  div.inner-modal {
+    position: fixed;
+    width: 50%;
+    height: auto;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: white;
+    display: flex;
+    flex-direction: column;
+    border-radius: 7px;
+
+    button.close-button {
+      outline: none;
+      border: none;
+      background: none;
+      margin-right: 10px;
+      margin-left: auto;
+      width: 20px;
+      height: 20px;
+      font-weight: bold;
+      color: #ff5a5f;
+      font-size: 25px;
+
+      :hover {
+        cursor: pointer;
+        opacity: 80%;
+      }
+    }
+  }
+`;
